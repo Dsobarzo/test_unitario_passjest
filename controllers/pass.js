efe075065017f64c4fe7ef7af790e31601f398ee
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2021-04-24 14:03:18 -0400

    Create pass.js

---
+++ controllers/pass.js
@@ -0,0 +1,25 @@
+const bcrypt = require("bcrypt")
+
+const genpass = (unhash_pass) => {
+    /*bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            return
+        }
+        console.log(salt)
+        bcrypt.hash(unhash_pass, salt, (err, secure_pass) => {
+            if (err) {
+                return
+            }
+            console.log(secure_pass)
+            return secure_pass
+        })
+    })*/
+    const salt = bcrypt.genSaltSync(12)
+    const secure_pass = bcrypt.hashSync(unhash_pass, salt)
+    return secure_pass;
+}
+
+//genpass("hola Mundo")
+console.log(genpass("Hola Mundo"))
+
+module.exports = { genpass }
