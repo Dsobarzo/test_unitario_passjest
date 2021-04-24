2d1678e086c827899dacdad53046bd3afc6b2aa7
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2021-04-24 14:45:17 -0400

    Update pass.test.js

--- __test__/pass.test.js
+++ __test__/pass.test.js
@@ -2,7 +2,7 @@ const { genpass } = require('../controllers/pass')
 
 describe("Pruebas sobre funcion GenPass", () => {
     it("Debe genera Contraseñas diferentees", () => {
-        const passp = "HolaMundo"
+        const passp = "Hola12"
         const pass1 = genpass(passp)
         const pass2 = genpass(passp)
         expect(pass1).not.toBe(pass2)
